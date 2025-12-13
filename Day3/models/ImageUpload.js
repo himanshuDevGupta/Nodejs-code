876962a9e0728549d212d658313eace339b017d5
Himanshu Gupta <himanshu.nikhil.gupta@gmail.com>
2025-12-13 14:39:03 +0530

    Fix invalid filename with trailing space (Windows safe)

--- Day3/models/ImageUpload.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var UserSchema = new mongoose.Schema({
-   image_url: String,
-   name:String,
-}, {timestamps: true});
-
-module.exports =mongoose.model('ImageUpload', UserSchema);
